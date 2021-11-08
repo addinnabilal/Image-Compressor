e9ee1bc3f362d18c4743047f98ffd26b4a2a3a5f
gagaspbahar <gagaspbahar@gmail.com>
2021-11-08 22:33:43 +0700

    Testing compress alg

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask.wrappers import Request
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Start")
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!"
+
+@app.route('/upload', methods=['POST'])
+def handleFileUpload():
+    pass
