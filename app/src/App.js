a37780232f4fed23f72bfdabc35cc5d04ac72a37
gagaspbahar <gagaspbahar@gmail.com>
2021-11-09 16:23:18 +0700

    Connecting backend to frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,46 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, InputGroup, Navbar, NavbarBrand, Input, Card, CardBody,  CardTitle, CardImg, Row, Col, InputGroupText, Container } from 'reactstrap';
 import Dropzone from 'react-dropzone';
 
 function App() {
-  const [image, setImage]=useState(null)
-  const [compressRate, setCompressRate]=useState(null)
-
-  const handleDrop = acceptedFiles => {
-    setImage(URL.createObjectURL(acceptedFiles[0]));
+  const [image, setImage]=useState(null);
+  const [compressRate, setCompressRate]=useState(null);
+  // const handleDrop = acceptedFiles => {
+  //   // console.log(acceptedFiles)
+  //   setImage(URL.createObjectURL(acceptedFiles[0]));
+  // };
+  const handleChange = function(e){
+    console.log("k = " + e.target.value);
+    setCompressRate(e.target.value);
   }
+
+  const handleUploadImage = function(ev){
+    // ev.preventDefault();
+    setImage(ev[0]);
+    const data = new FormData();
+    // console.log(compressRate);
+    // console.log(ev)
+    data.append('file', image);
+    data.append('k', compressRate);
+
+    fetch('http://localhost:5000/upload', {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      console.log(response)
+    });}
+    
+  const handleCompress = function(){
+    console.log(image);
+    fetch('http://localhost:5000/download/' + image.name, {
+    method: 'GET',
+  }).then((res) => {
+    console.log("Get download url success.");
+    setImage(res.url)
+    // this.setState({imageURL:res.url})});
+  })};
    
 
   return (
@@ -28,7 +58,7 @@ function App() {
       <body>
         <div style={{margin:'50px'}}>
           <Container className="contDrop" >
-            <Dropzone accept='image/*' onDrop={handleDrop}>
+            <Dropzone accept='image/*' onDrop={handleUploadImage}>
               {({ getRootProps, getInputProps }) => (
                 <div {...getRootProps({ className: "dropzone" })}>
                   <input {...getInputProps()} />
@@ -39,10 +69,10 @@ function App() {
           </Container>
           <InputGroup size='sm' style={{marginTop:'20px'}}>
             <InputGroupText>Compression Rate</InputGroupText>
-            <Input type="number"/>
+            <Input type="number" onChange={handleChange}/>
             <InputGroupText>%</InputGroupText>
           </InputGroup>
-          <Button size='lg' style={{margin:'20px'}}>Compress</Button>
+          <Button size='lg' onClick={handleCompress} style={{margin:'20px'}}>Compress</Button>
           <Row>
             <Col>
               <Card>
