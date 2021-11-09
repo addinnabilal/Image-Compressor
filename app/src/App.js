631fb6519b0d6d61e00b9e78be0056bd29ea51c7
addinnabilal <16520342@std.stei.itb.ac.id>
2021-11-10 01:46:01 +0700

    Edit frontend v2

--- app/src/App.js
+++ app/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, InputGroup, Navbar, NavbarBrand, Input, Card, CardBody,  CardTitle, CardImg, Row, Col, InputGroupText, Container } from 'reactstrap';
 import Dropzone from 'react-dropzone';
-
+import { BiDownload, BiWrench } from "react-icons/bi";
 function App() {
   const [image, setImage]=useState(null)
   const [compressRate, setCompressRate]=useState(null)
 
   const handleDrop = acceptedFiles => {
     setImage(URL.createObjectURL(acceptedFiles[0]));
+  };
+  var compRate;
+  function compressFunc(){
+    compRate=document.getElementById("compRateInput").value;
+    document.getElementById("printCompRate").innerHTML = compRate;
   }
-   
 
   return (
     <div className="App">
@@ -26,23 +30,21 @@ function App() {
         </div>
       </header>
       <body>
-        <div style={{margin:'50px'}}>
-          <Container className="contDrop" >
-            <Dropzone accept='image/*' onDrop={handleDrop}>
-              {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps({ className: "dropzone" })}>
-                  <input {...getInputProps()} />
-                  <p style={{color:'white'}}>Drag and drop files, or click to select files</p>
-                </div>
-              )}
-            </Dropzone>
-          </Container>
+        <div style={{margin:'100px', marginTop:'10px'}}>
+          <Dropzone accept='image/*' onDrop={handleDrop} className="contDrop">
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps({ className: "dropzone" })}>
+                <input {...getInputProps()} />
+                <p style={{color:'white'}}>Drag and drop files, or click to select files</p>
+              </div>
+            )}
+          </Dropzone>
           <InputGroup size='sm' style={{marginTop:'20px'}}>
             <InputGroupText>Compression Rate</InputGroupText>
-            <Input type="number"/>
+            <Input type="number" id="compRateInput"/>
             <InputGroupText>%</InputGroupText>
           </InputGroup>
-          <Button size='lg' style={{margin:'20px'}}>Compress</Button>
+          <Button onClick={compressFunc} size='lg' style={{margin:'20px'}}><BiWrench/>  Compress</Button>
           <Row>
             <Col>
               <Card>
@@ -50,8 +52,10 @@ function App() {
                   <CardTitle>
                     Before
                   </CardTitle>
-                  <CardImg
-                  src={image}/>
+                  <Container className='imgCont'>
+                    <CardImg
+                    src={image} style={{maxHeight:'100%',objectFit:'contain', alignItems:'center', justifyContent:'content'}}/>
+                  </Container>
                 </CardBody>
               </Card>
             </Col>
@@ -61,15 +65,26 @@ function App() {
                   <CardTitle>
                     After
                   </CardTitle>
-                  <CardImg>
-
-                  </CardImg>
+                  <Container className='imgCont'>
+                    <CardImg 
+                    />
+                  </Container>
                 </CardBody>
               </Card>
             </Col>
           </Row>
+          <div style={{marginTop:'10px'}}>
+              <Container>
+                <p style={{color:'white'}}>Image pixel difference percentage <span id="printCompRate"></span>%</p>
+              </Container>
+              <Container>
+                <p style={{color:'white'}}>Image compression time<span id="printCompTime"></span>%</p>
+              </Container>
+              <Container>
+                <Button><BiDownload/>  Download</Button>
+              </Container>
+          </div>
         </div>
-        
       </body>
     </div>
   );
