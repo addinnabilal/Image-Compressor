92c7bfba01a2d536aa62b10f5f283ce0bec3c39d
Aira Thalca <13520101@std.stei.itb.ac.id>
2021-11-10 07:11:19 +0700

    update algoritma (faster)

--- src/compress.py
+++ src/compress.py
@@ -22,15 +22,15 @@ def svd1d(A, epsilon=1e-8):
         if abs(np.dot(now, temp)) > 1 - epsilon:
             return now
 
-def svd(A,k, epsilon=1e-8):
+def svd2(A,k, epsilon=1e-8):
     A = np.array(A, dtype=np.float64)
     m = A.shape[0]
     n = A.shape[1]
     SVD = []
     for i in range(k):
         A1 = A.copy()
-        for sig, u, v in SVD[:i]:
-            A1 -= sig * np.outer(u, v)
+        for sing, u, v in SVD[:i]:
+            A1 -= sing * np.outer(u, v)
         if m > n:
             v = svd1d(A1)
             u_unnorm = np.dot(A, v)
@@ -42,8 +42,39 @@ def svd(A,k, epsilon=1e-8):
             sigma = norm(v_unnorm)
             v = v_unnorm / sigma
         SVD.append((sigma, u, v))
-    sigs, us, vs = [np.array(x) for x in zip(*SVD)]
-    return us.T,sigs, vs
+    sing, us, vs = [np.array(x) for x in zip(*SVD)]
+    return us.T,sing, vs
+
+def svd(A, k, epsilon=1e-8):
+    A = np.array(A,dtype=np.float64)
+    m = A.shape[0]
+    n = A.shape[1]
+    X=A.copy()
+    if m>=n:
+        A = np.dot(A.T,A)
+        val = n
+    else:
+        A = np.dot(A,A.T)
+        val = m
+    now = np.random.rand(val, k)
+    now , r = np.linalg.qr(now)
+    for i in range(250):
+        temp = now
+        Z = np.dot(A,now)
+        now, R = np.linalg.qr(Z)
+        error = ((now-temp) ** 2).sum()
+        if error < epsilon:
+            break
+    idx = np.argsort(np.diag(R))[::-1]
+    sing=np.sqrt(np.diag(R)[idx])
+    now = now[:,idx]
+    if m<n:
+        u=now
+        v=np.dot(np.linalg.inv(np.diag(sing)),np.dot(u.T,X))
+    else:
+        v=now.T
+        u=np.dot(X,np.dot(v.T,np.linalg.inv(np.diag(sing))))
+    return u, sing, v
 
 def compress_image(img,k):
     print("processing...")
@@ -97,16 +128,18 @@ def arrangegs(img,r):
 start = time()
 image_name = input("Masukkan nama image: ")
 path = 'assets\\' + image_name
-img = Image.open(path)
+img = Image.open(r'C:\Users\airat\OneDrive\Pictures\wp\1095253.jpg')
 img = np.asarray(img)
 if(len(img.shape) > 2):
-    rr,rg,rb = compress_image(img,50)
+    rr,rg,rb = compress_image(img,70)
     result = arrangeRGB(img,rr,rg,rb)
 else:
     r = compress_image(img,50)
     result = arrangegs(img,r)
 image = Image.fromarray(result)
-filename = input("Masukkan nama file: ")
-path = 'assets\\' + filename
-image.save(path, 'JPEG')
+plt.imshow(image)
+plt.show()
+# filename = input("Masukkan nama file: ")
+# path = 'assets\\' + filename
+image.save(r'C:\Users\airat\OneDrive\Pictures\wp\1095253_2.jpg','JPEG')
 print(f'Time taken to run: {time() - start} seconds')
